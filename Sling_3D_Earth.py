6e29c913dd7370f3b3df5f065605f419efb542d1
Keshavthunder <111967884+Keshavthunder@users.noreply.github.com>
2026-01-30 21:29:59 -0500

    Update Sling_3D_Earth.py

--- Sling_3D_Earth.py
+++ Sling_3D_Earth.py
@@ -184,19 +184,12 @@ def main():
         grand_total = housing_per_person + transport_total + food_total + shopping_total
     
         st.markdown("### Grand Total")
-        st.metric("Your Annual Carbon Footprint", f"{grand_total:,.0f} kg $\\text{'CO'}_2$e")
+        #st.metric("Your Annual Carbon Footprint", f"{grand_total:,.0f} kg $\\text{'CO'}_2$e")
+        st.metric("Grand Total", f"{grand_total:,.0f} kg " + r"$\mathrm{CO}_2$e")
 
         earths = grand_total / 4000
         score = max(1, min(9, int(math.ceil(earths))))
-        """
-        data = {
-            "Housing": housing_per_person,
-            "Transport": transport_total,
-            "Food": food_total,
-            "Shopping": shopping_total
-        }
-        st.bar_chart(data)
-        """
+
 
         #score = st.slider("Select your impact level:", 1, 9, 4)
         st.subheader("If everyone lived like you, we would need:")
